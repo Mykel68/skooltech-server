2da80f6761bdcb29efd245a3b486e8e528f2cdff
Mplus <olowookeremicheal9568@gmail.com>
2025-08-13 15:44:06 +0100

    fix:for terms to having multiple roles

--- src/controllers/term.controller.ts
+++ src/controllers/term.controller.ts
@@ -27,7 +27,7 @@ export const createTerm = async (
   next: NextFunction
 ) => {
   try {
-    if (!req.user || req.user.role !== "Admin") {
+    if (!req.user?.role.includes("Admin")) {
       throw new AppError("Only admins can create terms", 403);
     }
 
@@ -72,7 +72,7 @@ export const updateSchoolTerm = async (
   try {
     const { school_id, session_id, term_id } = req.params;
 
-    if (req.user?.role !== "Admin") {
+    if (!req.user?.role.includes("Admin")) {
       throw new AppError("Only admins can update terms", 403);
     }
 
