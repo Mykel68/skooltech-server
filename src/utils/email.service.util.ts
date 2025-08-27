47f40cb71c7c1345fdf12f5608a93e35db6dc2a2
Mplus <olowookeremicheal9568@gmail.com>
2025-08-27 14:50:23 +0100

    chore:dotenv removed

--- src/utils/email.service.util.ts
+++ src/utils/email.service.util.ts
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 const transporter = nodemailer.createTransport({
 	host: process.env.SMTP_SERVER!,
